c3f45abcbf4549798927b48a678654237cb76239
LuisFelipe272009 <luis.piekarski@escola.pr.gov.br>
2025-05-27 08:51:13 -0300

    Add files via upload

---
+++ Exercício 1/script.js
@@ -0,0 +1,14 @@
+let num1;
+let num2;
+let resultado;
+let parouimpar;
+num1 = Number(window.prompt("Digite aqui seu primeiro número"));
+num2 = Number(window.prompt("Digite o segundo número"));
+
+resultado = num1 + num2;
+
+parouimpar = resultado % 2;
+
+if( parouimpar ==0) {
+    alert("O número" + resultado + "É par");
+}
